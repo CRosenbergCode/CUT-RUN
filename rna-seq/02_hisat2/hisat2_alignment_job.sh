8061418458977643ad03b53ac5a7a26d0236e665
Pegah Eizad <peizadkh@gmail.com>
2024-03-11 12:37:01 -0600

    script for hisat2 alignment

---
+++ rna-seq/02_hisat2/hisat2_alignment_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --partition=amem
+#SBATCH --job-name=alignmentJob
+#SBATCH --output=%x.%j.out
+#SBATCH --time=10:00:00
+#SBATCH --qos=mem
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=pegah.eizadkhah@colostate.edu
+
+module purge
+module load anaconda
+conda activate cutruntools2.1
+
+#directory for sequenced sample data: /projects/haogg@colostate.edu/RosenbergShared/datasets/CUTRUNResults/raw/20240221_LH00407_0020_B22HHNNLT3/RNA
+hisat2 -p 16 --rna-strandness --phred33 -x aedesA_tran -1 /projects/haogg@colostate.edu/RosenbergShared/datasets/CUTRUNResults/raw/20240221_LH00407_0020_B22HHNNLT3/RNA/RNA-BF_Rep2_S20_L006_R1_001.fastq.gz -2 /projects/haogg@colostate.edu/RosenbergShared/datasets/CUTRUNResults/raw/20240221_LH00407_0020_B22HHNNLT3/RNA/RNA-BF_Rep2_S20_L006_R2_001.fastq.gz -S realData/alignmentSummary_bf_rep2.sam
