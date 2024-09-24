c69c0d5f340a713c809dff651a9743f3e5135d2a
haogg <hunter.a.ogg@gmail.com>
2024-09-24 14:36:14 -0600

    Newer, cleaner version of

--- dna-seq/04_Diffbind/DiffBindFunction.R
+++ dna-seq/04_Diffbind/DiffBindFunction.R
@@ -20,37 +20,44 @@ library(GenomicRanges)
 library(Repitools)
 #samples <- read.csv('chipSamples2_28_24.csv')
 
-runDiffbind = function(sampleCSV,samples1,samples2,plotting=FALSE,saving=TRUE,summits=FALSE,sampleName=FALSE,
-                       namesOne="Condition1",namesTwo="Condition2",fromFile=FALSE,CSV=TRUE){
-  samplesOnly <- read.csv(sampleCSV)
-  dbOnly <- dba(sampleSheet=samplesOnly) #scoreCol=5
-  resultOnly <- dba.count(dbOnly,summits=summits)
-  #saveRDS(resultOnly,file="onlyPeakCounts.RDS")
-  resultOnly=readRDS("onlyPeakCounts.RDS")
-  resultOnly
-  testPeaks=dba.peakset(resultOnly)
-  normOnly <- dba.normalize(resultOnly)
+runDiffbind = function(sampleFile,samples1,samples2,plotting=FALSE,saving=TRUE,summits=FALSE,sampleName=FALSE,
+                       namesOne="Condition1",namesTwo="Condition2",fromFile=FALSE,CSV=TRUE,edger=TRUE){
   #contrastOnly <- dba.contrast(normOnly, minMembers=2)
   
   #resultOnly <- dba.count(dbOnly,summits = FALSE)#,summits=20000)
   #if
   #saveRDS(resultOnly,file="onlySummitFalseCounts.RDS")
   if(fromFile){
-    constastOnly=readRDS(fromFile)
+    contrastOnly=readRDS(sampleFile)
   }
   else{
+    samplesOnly <- read.csv(sampleFile)
+    dbOnly <- dba(sampleSheet=samplesOnly) #scoreCol=5
+    resultOnly <- dba.count(dbOnly,summits=summits)
+    #saveRDS(resultOnly,file="onlyPeakCounts.RDS")
+    if(saving){
+      saveRDS(resultOnly,file=paste(namesOne,"vs",namesTwo,"PeakCounts.RDS",sep = ""))
+    }
+    #resultOnly=readRDS("onlyPeakCounts.RDS")
+    #resultOnly
+    testPeaks=dba.peakset(resultOnly)
+    normOnly <- dba.normalize(resultOnly)
     contrastOnly <- dba.contrast(normOnly,group1=samples1, group2=samples2,
                                  name1=namesOne, name2=namesTwo,minMembers = 2)
   }
 
-  analizOnly <- dba.analyze(contrastOnly,method=DBA_ALL_METHODS)
+  analizOnly <- dba.analyze(contrastOnly,method=DBA_ALL_METHODS,bBlacklist = FALSE,bGreylist = FALSE)
 
   #analiz <- dba.analyze(contrastOnly,method=DBA_ALL_METHODS)
 
   #pvals <- dba.plotBox(analizOnly)
-  
-  res_deseq <- dba.report(analizOnly, method=DBA_EDGER, contrast = 1, th=1)
-  
+  if(edger){
+    res_deseq <- dba.report(analizOnly, method=DBA_EDGER, contrast = 1, th=1)
+  }
+  else{
+    res_deseq <- dba.report(analizOnly, method=DBA_DESEQ2, contrast = 1, th=1)
+  }
+
   #head(n=25,res_deseq)
   
   #head(res_deseq,n=9)
@@ -60,9 +67,9 @@ runDiffbind = function(sampleCSV,samples1,samples2,plotting=FALSE,saving=TRUE,su
   #sigResDF = head(resDF,n=25,stringsAsFactors=FALSE)
   if(saving){
     if(CSV){
-      write.csv(resDF,paste(namesOne,"vs",namesTwo), row.names=FALSE)
+      write.csv(resDF,paste(namesOne,"vs",namesTwo,".RDS",sep=""), row.names=FALSE)
     }
-    saveRDS(resDF,paste(namesOne,"vs",namesTwo))
+    saveRDS(resDF,paste(namesOne,"vs",namesTwo,".RDS",sep=""))
   }
   
   if(plotting){
@@ -83,202 +90,40 @@ runDiffbind = function(sampleCSV,samples1,samples2,plotting=FALSE,saving=TRUE,su
     
     plot(olap.rate,type='b',ylab='# peaks', xlab='Overlap at least this many peaksets')
   }
-  
+  return(resDF)
 }
 
 
-runDiffbind("chipSamplesOnlyPair.csv")
+runDiffbind(sampleFile="chipSamplesOnlyPair.csv",samples1 = c(1:3),samples2=c(4:6))
 
-######
-#MACS 2 Only Paired End
+test=readRDS("Condition1vsCondition2.RDS")
 
-samplesOnly <- read.csv('chipSamplesOnlyPair.csv')
-samplesOnly
-dbOnly <- dba(sampleSheet=samplesOnly) #scoreCol=5
-dbOnly["Intervals"]
-#resultOnly <- dba.count(dbOnly,summits=FALSE)#,summits=20000)
-#saveRDS(resultOnly,file="onlyPeakCounts.RDS")
-resultOnly=readRDS("onlyPeakCounts.RDS")
-resultOnly
-testPeaks=dba.peakset(resultOnly)
-normOnly <- dba.normalize(resultOnly)
-contrastOnly <- dba.contrast(normOnly, minMembers=2)
+runDiffbind(sampleFile="Condition1vsCondition2PeakCounts.RDS",fromFile = TRUE)
 
-testPeaks$peaks[3]
-testPeaks$samples
+testFile=readRDS("Condition1 vs Condition2")
+#
+#
+#It is highly recommend to save
 
-#resultOnly <- dba.count(dbOnly,summits = FALSE)#,summits=20000)
-#saveRDS(resultOnly,file="onlySummitFalseCounts.RDS")
+#runDiffbind = function(sampleCSV="",samples1,samples2,plotting=FALSE,saving=TRUE,summits=FALSE,sampleName=FALSE,
+#namesOne="Condition1",namesTwo="Condition2",fromFile=FALSE,CSV=TRUE,edger=TRUE){
 
-#https://support.bioconductor.org/p/108094/
-#resultOnly$peaks
-#normOnly
-contrastOnly <- dba.contrast(normOnly,group1=c(1:2,7:8), group2=c(3:4,9:11),
-                             name1="K9Me3", name2="K27Ac",minMembers = 2)
+#namesOne and namesTwo are not necessary to provide, they are labels
+#Using them helps properly keep track of the comparison groups (i.e. RVFV Ac vs. BF Ac)
 
-contrastOnly <- dba.contrast(normOnly,group1=c(1:2,5:8,12:13), group2=c(3:4,9:11),
-                             name1="All_Others", name2="K27Ac")#,minMembers = 2)
+#'samples1' and 'samples2' are indices which indicate the samples to compare. 
+#The numbers are equivalent to rows in the csv or dataframe, excluding headers
+#As a reminder, R is 1 indexed so the first object will be 1
 
-contrastOnly <- dba.contrast(normOnly,group1=c(1:2,7:8), group2=c(3:6,9:13),
-                             name1="K9Me3", name2="All_Others")#,minMembers = 2)
+#'plotting' simply determines whether or not plots will be displayed
+#It is turned off by default
 
-#saveRDS(contrastOnly,"AcVsAllCHIP.rds")
-#saveRDS(contrastOnly,"MeVsAllCHIP.rds")
-contrastOnly = readRDS("AcVsAllCHIP.rds")
+#'saving' allows the user to save intermediate files such as 
+#Take care as if the name is the same as a previous file THE OLD FILE WILL BE OVERWRITTEN
+#If the file summary file is to be saved, users should do this themselves.
 
-#contrastOnly <- dba.contrast(normOnly,group1=3:4, group2=9:11,
-                             #name1="K9Me3 BF", name2="K9Me3 RVFV",minMembers = 2)
-
-contrastOnly <- dba.contrast(normOnly,group1=3:4, group2=9:11,
-                            name1="K27Ac BF", name2="K27Ac RVFV",minMembers = 2)
-
-
-analizOnly <- dba.analyze(contrastOnly,method=DBA_ALL_METHODS)
-par("mar")
-par(mar=c(1,1,1,1))
-analizOnly
-dba.plotVenn(analizOnly,contrast=1,method=DBA_ALL_METHODS)
-
-#analiz <- dba.analyze(contrastOnly,method=DBA_ALL_METHODS)
-
-dba.plotPCA(analizOnly,  attributes=DBA_FACTOR, label=DBA_ID)
-
-plot(analizOnly)
-
-dba.plotVolcano(analizOnly)
-
-dba.plotMA(analizOnly, method=DBA_DESEQ2)
-
-dba.plotMA(analizOnly, bXY=TRUE)
-
-pvals <- dba.plotBox(analizOnly)
-
-res_deseq <- dba.report(analizOnly, method=DBA_EDGER, contrast = 1, th=1)
-
-summary(resultOnly$binding[,3]-resultOnly$binding[,2])
-
-head(n=25,res_deseq)
-
-head(res_deseq,n=9)
-
-library(GenomicRanges)
-resDF = annoGR2DF(res_deseq)
-
-sigResDF = head(resDF,n=25,stringsAsFactors=FALSE)
-
-
-write.csv(sigResDF, "macs2EDGER_Ac.csv", row.names=FALSE)
-
-
-#saveRDS(sigResDF,"AcVsAllResDF.RDS")
-
-saveRDS(sigResDF,"AcBFVsAcRVFVDF.RDS")
-
-olap.rate <- dba.overlap(dbOnly, mode=DBA_OLAP_RATE)
-olap.rate
-plot(olap.rate,type='b',ylab='# peaks', xlab='Overlap at least this many peaksets')
-
-
-######
-#MACS 2 Strict Paired End
-
-
-samplesStrict <- read.csv('chipSamplesStrictPair.csv')
-dbStrict <- dba(sampleSheet=samplesStrict) #scoreCol=5
-#resultStrict <- dba.count(dbStrict)#,summits=20000)
-#saveRDS(resultStrict,file="strictPeakCounts.RDS")
-resultStrict=readRDS("strictPeakCounts.RDS")
-dba.peakset(resultStrict)
-normStrict <- dba.normalize(resultStrict)
-contrastStrict <- dba.contrast(normStrict, minMembers=2)
-
-#https://support.bioconductor.org/p/108094/
-#resultStrict$peaks
-#normStrict
-contrastStrict <- dba.contrast(normStrict,group1=3:4, group2=9:11,
-                             name1="K9Me3 BF", name2="K9Me3 RVFV",minMembers = 2)
-analizStrict <- dba.analyze(contrastStrict,method=DBA_ALL_METHODS)
-par("mar")
-par(mar=c(1,1,1,1))
-analizStrict
-dba.plotVenn(analizStrict,contrast=1,method=DBA_ALL_METHODS)
-
-analiz <- dba.analyze(contrastStrict,method=DBA_ALL_METHODS)
-
-dba.plotPCA(analizStrict,  attributes=DBA_FACTOR, label=DBA_ID)
-
-plot(analizStrict)
-
-dba.plotVolcano(analizStrict)
-
-dba.plotMA(analizStrict, method=DBA_DESEQ2)
-
-dba.plotMA(analizStrict, bXY=TRUE)
-
-pvals <- dba.plotBox(analizStrict)
-
-res_deseq <- dba.report(analizStrict, method=DBA_EDGER, contrast = 1, th=1)
-
-res_deseq
-
-olap.rate <- dba.overlap(dbStrict, mode=DBA_OLAP_RATE)
-olap.rate
-plot(olap.rate,type='b',ylab='# peaks', xlab='Overlap at least this many peaksets')
-
-
-######
-#SEACR Input Relaxed
-
-samplesInput <- read.csv('seacrNormed.csv')
-dbInput <- dba(sampleSheet=samplesInput) #scoreCol=5
-resultInput <- dba.count(dbInput,summits=FALSE)#,summits=20000)
-#saveRDS(resultInput,file="inputPeakCounts.RDS")
-resultInput=readRDS("inputPeakCounts.RDS")
-resultInput
-dba.peakset(resultInput)
-normInput <- dba.normalize(resultInput)
-contrastInput <- dba.contrast(normInput, minMembers=2)
-
-#https://support.bioconductor.org/p/108094/
-#resultInput$peaks
-#normInput
-contrastInput <- dba.contrast(normInput,group1=3:4, group2=9:11,
-                             name1="K9Me3 BF", name2="K9Me3 RVFV",minMembers = 2)
-analizInput <- dba.analyze(contrastInput,method=DBA_ALL_METHODS)
-par("mar")
-par(mar=c(1,1,1,1))
-analizInput
-dba.plotVenn(analizInput,contrast=1,method=DBA_EDGER)
-
-dba.plotPCA(analizInput,attributes=DBA_FACTOR, label=DBA_ID)
-
-plot(analizInput)
-
-dba.plotVolcano(analizInput)
-
-dba.plotMA(analizInput, method=DBA_DESEQ2)
-
-dba.plotMA(analizInput, bXY=TRUE)
-
-pvals <- dba.plotBox(analizInput)
-
-res_deseq <- dba.report(analizInput, method=DBA_EDGER, contrast = 1, th=1)
-
-res_deseq
-
-saveRDS(res_deseq,file="SEACRRelaxedPairedOnlyEdgeR_Me_DB.RDS")
-res_deseq=readRDS("SEACRRelaxedPairedOnlyEdgeR_Me_DB.RDS")
-
-seqnames = res_deseq[,1][,1]
-res_deseq["Conc"]
-library(GenomicRanges)
-resDF = annoGR2DF(res_deseq)
-sigACDF = head(resDF,n=139,stringsAsFactors=FALSE)
-#pAdj = res_deseq$FDR
-
-write.csv(sigResDF, "seacrEDGER_Ac.csv", row.names=FALSE)
-
-
-olap.rate <- dba.overlap(dbInput, mode=DBA_OLAP_RATE)
-olap.rate
-plot(olap.rate,type='b',ylab='# peaks', xlab='Overlap at least this many peaksets')
+#'fromFile' is a boolean which indicates whether the file provided 
+#should be interpreted as a an RDS file holding the counts object
+#This is mainly to save time, as the process can
+#The samples compared in the contrast can change, but expected peak size
+#CANNOT be changed when reading from a file
