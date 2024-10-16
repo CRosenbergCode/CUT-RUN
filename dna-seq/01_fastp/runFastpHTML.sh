1862a73602e504e6b0614620b92a1aa87d2f5179
meekrob <davidcking.inbox@gmail.com>
2024-10-16 10:34:26 -0600

    runFastpHTML.sh: added basename subcommands, modified conda invocation

--- dna-seq/01_fastp/runFastpHTML.sh
+++ dna-seq/01_fastp/runFastpHTML.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
-
 #SBATCH --partition=day-long-cpu
 #SBATCH --job-name=fastpLoop
 #SBATCH --output=%x.%j.out
 #SBATCH --time=1:00:00
 #SBATCH --qos=normal
 #SBATCH --nodes=1
-#SBATCH --ntasks=6
+#SBATCH --ntasks=17
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=$USER
 
 module purge
-source activate base
-conda activate rnaPseudo
+#source activate base
+#conda activate rnaPseudo
+source $HOME/miniconda3/bin/activate cutrun
+
+mkdir -vp trimmed htmls
 
 for FILE in raw/*R1_001.fastq.gz
 do
   echo $FILE
   #TWO=$(echo $FILE | rev | sed s/./2/14 | rev) #Read in R1 file, reverse it, replace the 12th character with a 2, and reverse for R2
   TWO=${FILE/R1/R2}
-  TRIM1="trimmed/trimmed."${FILE}
-  TRIM2="trimmed/trimmed."${TWO}
-  fastp -i ${FILE} -I ${TWO} -o ${TRIM1} -O ${TRIM2} -h htmls/${TWO}.html -w 5
-  #increase -w to increase number of threads, but be sure to increase the number of threas as well
+  TRIM1="trimmed/trimmed."$(basename $FILE)
+  TRIM2="trimmed/trimmed."$(basename $TWO)
+  # fastp uses 16 threads max (version 0.23.4)
+  cmd="fastp -i ${FILE} -I ${TWO} -o ${TRIM1} -O ${TRIM2} -h htmls/$(basename $TWO).html -w $((SLURM_NTASKS-1)) --dedup"
+  echo $cmd
+  time eval $cmd
 done
